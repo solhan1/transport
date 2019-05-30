c00fef726b97d6f8dccfadea46969ddb1394b733
Sol Han <shan@krg.ca>
2019-05-30 12:29:37 -0400

    Implemented getting fields from table cells. TODO: FORMTEXT

--- TransportAutomation/TransportAutomation/Program.cs
+++ TransportAutomation/TransportAutomation/Program.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TransportAutomation.src.DocumentProcessors.docx;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using DocumentFormat.OpenXml.Office.CustomUI;
+using System.IO;
+using System.Windows.Forms;
+
 
 namespace TransportAutomation
 {
@@ -10,6 +17,55 @@ namespace TransportAutomation
     {
         static void Main(string[] args)
         {
+            DocumentProcessor d = new DocumentProcessor();
+            string path = Directory.GetCurrentDirectory();
+            foreach (string fileName in Directory.GetFiles(path))
+            {
+                string fileExtension = Path.GetExtension(fileName);
+                if (fileExtension == ".docx")
+                {
+                    try
+                    {
+                        WordprocessingDocument document = d.openWordDocument(fileName, true);
+                        // Body docBody = d.getWordDocumentBody(document);
+                        TableCell cell0 = d.DAIRCellGetter(document, 3, 0, 0);
+                        TableCell cell1 = d.DAIRCellGetter(document, 3, 0, 1);
+                        TableCell cell2 = d.DAIRCellGetter(document, 0, 2, 2);
+                        string text0 = d.DAIRCellTextGetter(cell0);
+                        string text1 = d.DAIRCellTextGetter(cell1);
+                        string text2 = d.DAIRCellTextGetter(cell2);
+                        //Table table = document.MainDocumentPart.Document.Body.Elements<Table>().ElementAt(3);
+                        //TableRow row = table.Elements<TableRow>().ElementAt(0);
+                        //TableCell cell0 = row.Elements<TableCell>().ElementAt(0);
+                        //TableCell cell1 = row.Elements<TableCell>().ElementAt(1);
+                        //DropDownListFormField dd = cell0.Elements<DropDownListFormField>();
+                        Console.WriteLine(text0);
+                        Console.WriteLine(text1);
+                        Console.WriteLine(text2);
+                        // commented out code for writing to word doc
+                        //Paragraph para = docBody.AppendChild(new Paragraph());
+                        //Run run = para.AppendChild(new Run());
+                        //run.AppendChild(new Text("Append text in body, but text is not saved - OpenWordprocessingDocumentReadonly"));
+                        //document.MainDocumentPart.Document.Save();
+                    }
+                    catch (FileNotFoundException e)
+                    {
+                        MessageBox.Show("ERROR: The file:" + fileName + "was not found");
+                    }
+                    catch (IOException e)
+                    {
+                        MessageBox.Show("ERROR: Documents to be processed must not be open. Please close them and try again.");
+                        Environment.Exit(-1);
+                    }
+                    catch (Exception e)
+                    {
+                        MessageBox.Show("ERROR: Uncaught error, please contact IT. " + e);
+                    }
+                }
+                
+            }
+            
+            
         }
     }
 }
---
+++ TransportAutomation/TransportAutomation/src/DocumentProcessors/docx/DocumentProcessor.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Packaging;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace TransportAutomation.src.DocumentProcessors.docx
+{
+    public class DocumentProcessor
+    {
+        public DocumentProcessor()
+        {
+        }
+
+        // opens a .docx using a path.
+        // filePath: the location of the file
+        // readOnly: false = read-only, true otherwise 
+        public WordprocessingDocument openWordDocument(string filePath, bool readOnly)
+        {
+            WordprocessingDocument document = WordprocessingDocument.Open(filePath, readOnly);
+            return document;
+        }
+
+        // gets the body of an open .docx file
+        // document: an instance of the .docx file opened using openWordDocument()
+        public Body getWordDocumentBody(WordprocessingDocument document)
+        {
+            Body body = document.MainDocumentPart.Document.Body;
+            return body;
+        }
+
+        public Table tableGetter(WordprocessingDocument document, int tableIndex)
+        {
+            Table table = document.MainDocumentPart.Document.Body.Elements<Table>().ElementAt(tableIndex);
+            return table;
+        }
+
+        public TableRow tableRowGetter(Table table, int rowIndex)
+        {
+            TableRow row = table.Elements<TableRow>().ElementAt(rowIndex);
+            return row;
+        }
+
+        public TableCell tableCellGetter(TableRow row, int cellIndex)
+        {
+            TableCell cell = row.Elements<TableCell>().ElementAt(cellIndex);
+            return cell;
+        }
+        public TableCell DAIRCellGetter(WordprocessingDocument document, int tableIndex, int rowIndex, int cellIndex)
+        {
+            Table table = tableGetter(document, tableIndex);
+            TableRow row = tableRowGetter(table, rowIndex);
+            TableCell cell = tableCellGetter(row, cellIndex);
+            return cell;
+        }
+
+        public string DAIRCellTextGetter(TableCell cell)
+        {
+            if (cell.InnerText.Contains("FORMDROPDOWN"))
+            {
+                DropDownListFormField dropdown = cell.Elements<Paragraph>().First().Elements<Run>().First().Elements<FieldChar>().First().Elements<FormFieldData>().First().Elements<DropDownListFormField>().First();
+                int selectedIndex = dropdown.DropDownListSelection.Val;
+                ListEntryFormField selected = dropdown.Elements<ListEntryFormField>().ElementAt(selectedIndex);
+                string selectedText = selected.Val;
+                return selectedText;
+            }
+            else
+            {
+                return cell.InnerText;
+            }
+        }
+    }
+}
